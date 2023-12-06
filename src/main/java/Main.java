8c4b92657a7fbdffc9c0676afa76c1f4e435ba99
seeneemp3 <seeneemp3@gmail.com>
2023-12-06 13:01:50 +0400

    first choice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,15 @@ public class Main {
             new Door("3", false, false),
     };
     public static void main(String[] args) throws InterruptedException  {
+        //if someone hide a treasure behind random door
+        //what probability to find it at first try
+        firstChoice(doors[0], 1000);
 
-        progress(doors[0].isRight);
+
     }
-    public static void progress(boolean istRight) throws InterruptedException{
+    public static void firstChoice(Door door, int iterations) throws InterruptedException{
         double marks = 0;
         int percent = 0;
-        int iterations = 10000;
 
         System.out.print("Calculating: [");
         for (int i = 0; i < 100; i++) {
@@ -27,64 +29,21 @@ public class Main {
             doors[random].setRight(true);
             System.out.print("Calculating: [");
 
-            if(doors[0].isRight){
+            if(door.isRight){
                 marks ++;
-                 percent = (int)((marks / iterations) * 100);
+                percent = (int)((marks / iterations) * 100);
             }
             for (int j = 0; j < percent; j++) {
                 System.out.print("*");
             }
-
             for (int j = percent; j < 100; j++) {
                 System.out.print(" ");
             }
 
             doors[random].setRight(false);
             System.out.print("] " + percent + "%");
-            Thread.sleep(0);
+            //Thread.sleep(80);
             System.out.print("\r");
         }
-
-
-//        int total = 100;
-//        int width = 50;
-//
-//        System.out.print("Calculating: [");
-//        for (int i = 0; i < width; i++) {
-//            System.out.print(" ");
-//        }
-//        System.out.print("]\r");
-//
-//        for (int i = 0; i <= 100; i++) {
-//            int progress = width * i / total;
-//
-//            System.out.print("Calculating: [");
-
-//            for (int j = 0; j < progress; j++) {
-//                System.out.print("*");
-//            }
-//            for (int j = progress; j < width; j++) {
-//                System.out.print(" ");
-//            }
-//            System.out.print("] " + i + "%");
-//
-//            Thread.sleep(80);
-//            System.out.print("\r");
-//        }
-    }
-    public static void firstChoice() throws InterruptedException {
-        //if someone hide a treasure behind random door
-
-//        System.out.println(doors[0].toString());
-//        System.out.println(doors[1].toString());
-//        System.out.println(doors[2].toString());
-
-
-        //what probability to find it at first try
-
-
-        //System.out.print((int)(Math.random() * 3) + " ");
-
-
     }
 }
